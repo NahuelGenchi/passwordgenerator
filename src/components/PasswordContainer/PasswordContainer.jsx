83e26227584fd3a12eb2d1740476b66cc2aba865
Nahuel Genchi <65604505+NahuelGenchi@users.noreply.github.com>
2022-09-28 00:00:29 -0300

    adding styles to PasswordContainer

--- src/components/PasswordContainer/PasswordContainer.jsx
+++ src/components/PasswordContainer/PasswordContainer.jsx
@@ -2,19 +2,29 @@ import "./PasswordContainer.scss";
 
 const PasswordContainer = () => {
   return(
-    <div className="container">
+    <div className="password-container">
       <h1>Password Generator</h1>
       <h3>password</h3>
-      <form>
-        <label htmlFor="">Number of characters</label>
-        <input type="range" name="" id="characterAmountRange" min={1} max={50} value={10}/>
-        <input type="number" name="" id="characterAmountNumber" min={1} max={50} value={10}/>
-        <label htmlFor="includeUppercase">Include uppercase</label>
-        <input type="checkbox" name="" id="includeUppercase" />
-        <label htmlFor="includeNumbers">Include numbers</label>
-        <input type="checkbox" name="" id="includeNumbers" />
-        <label htmlFor="includeSymbols">Include symbols</label>
-        <input type="checkbox" name="" id="includeSymbols" />
+      <form className="password-c-form">
+        <div>
+          <label htmlFor="">Number of characters</label>
+          <input type="number" name="" id="characterAmountNumber" min={1} max={50} value={10}/>
+          <div>
+            <input type="range" name="" id="characterAmountRange" min={1} max={50} value={10}/>
+          </div>
+        </div>
+        <div>
+          <input type="checkbox" name="" id="includeUppercase" />
+          <label htmlFor="includeUppercase">Include uppercase</label>
+        </div>
+       <div>
+          <input type="checkbox" name="" id="includeNumbers" />
+          <label htmlFor="includeNumbers">Include numbers</label>
+       </div>
+        <div>
+          <input type="checkbox" name="" id="includeSymbols" />
+          <label htmlFor="includeSymbols">Include symbols</label>
+        </div>
         <button type="submit">Generate password</button>
       </form>
     </div>
