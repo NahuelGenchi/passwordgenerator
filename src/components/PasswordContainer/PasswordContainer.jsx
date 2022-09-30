f7194ac4f314effef96698d596356c65e03b6016
Nahuel Genchi <65604505+NahuelGenchi@users.noreply.github.com>
2022-09-29 23:20:37 -0300

    adding handleEvents to inputs

--- src/components/PasswordContainer/PasswordContainer.jsx
+++ src/components/PasswordContainer/PasswordContainer.jsx
@@ -4,16 +4,33 @@ import "./PasswordContainer.scss";
 
 const PasswordContainer = () => {
   const [inputValue, setInputValue] = useState(1);
+  const [includeUppercase, setIncludeUppercase] = useState(false);
+  const [includeNumbers, setIncludeNumbers] = useState(false);
+  const [includeSymbols, setIncludeSymbols] = useState(false);
 
   const handleInput = (e) => {
     setInputValue(e.target.value);
+  };
+
+  const handleChangeUpper = (e) => {
+    e.target.checked ? setIncludeUppercase(true) : setIncludeUppercase(false);
+  };
+  const handleChangeNumbers = (e) => {
+    e.target.checked ? setIncludeNumbers(true) : setIncludeNumbers(false);
+  };
+  const handleChangeSymbols = (e) => {
+    e.target.checked ? setIncludeSymbols(true) : setIncludeSymbols(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
   };
 
   return(
     <div className="password-container">
       <h1>Password Generator</h1>
       <h3>password</h3>
-      <form className="password-c-form">
+      <form className="password-c-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="">Number of characters</label>
           <input onChange={handleInput} type="number" name="" id="characterAmountNumber" min={1} max={50} value={inputValue}/>
@@ -22,15 +39,15 @@ const PasswordContainer = () => {
           </div>
         </div>
         <div>
-          <input type="checkbox" name="" id="includeUppercase" />
+          <input type="checkbox" name="" id="includeUppercase" onChange={handleChangeUpper}/>
           <label htmlFor="includeUppercase">Include uppercase</label>
         </div>
        <div>
-          <input type="checkbox" name="" id="includeNumbers" />
+          <input type="checkbox" name="" id="includeNumbers" onChange={handleChangeNumbers}/>
           <label htmlFor="includeNumbers">Include numbers</label>
        </div>
         <div>
-          <input type="checkbox" name="" id="includeSymbols" />
+          <input type="checkbox" name="" id="includeSymbols" onChange={handleChangeSymbols}/>
           <label htmlFor="includeSymbols">Include symbols</label>
         </div>
         <button type="submit">Generate password</button>
