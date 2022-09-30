5fa36774a371eca7eccc2c7e2a0ac4b56c202524
Nahuel Genchi <65604505+NahuelGenchi@users.noreply.github.com>
2022-09-30 00:08:11 -0300

    generating random password

--- src/components/PasswordContainer/PasswordContainer.jsx
+++ src/components/PasswordContainer/PasswordContainer.jsx
@@ -7,6 +7,7 @@ const PasswordContainer = () => {
   const [includeUppercase, setIncludeUppercase] = useState(false);
   const [includeNumbers, setIncludeNumbers] = useState(false);
   const [includeSymbols, setIncludeSymbols] = useState(false);
+  const [password, setPassword] = useState();
 
   const handleInput = (e) => {
     setInputValue(e.target.value);
@@ -22,14 +23,46 @@ const PasswordContainer = () => {
     e.target.checked ? setIncludeSymbols(true) : setIncludeSymbols(false);
   };
 
+  const arrayFromLowToHigh = (low, high) => {
+    const arr = [];
+    for (let i = low; i <= high; i++) {
+      arr.push(i);
+    };
+    return arr;
+  };
+  const lowerCaseCharCodes = arrayFromLowToHigh(65, 90);
+  const upperCaseCharCodes = arrayFromLowToHigh(97, 122);
+  const numberCharCodes = arrayFromLowToHigh(48, 57);
+  const symbolCharCodes = arrayFromLowToHigh(33, 47).concat(arrayFromLowToHigh(58, 64)).concat(arrayFromLowToHigh(91, 96)).concat(arrayFromLowToHigh(123, 126));
+
+  const generatePassword = (characterAmount, uppercase, numbers, symbols) => {
+    let charCodes = lowerCaseCharCodes;
+    if (uppercase) {
+      charCodes = charCodes.concat(upperCaseCharCodes);
+    };
+    if (numbers) {
+      charCodes = charCodes.concat(numberCharCodes);
+    };
+    if (symbols) {
+      charCodes = charCodes.concat(symbolCharCodes);
+    };
+    const passwordCharacters = [];
+    for (let i = 0; i < characterAmount; i++) {
+      const character = charCodes[Math.floor(Math.random() * charCodes.length)];
+      passwordCharacters.push(String.fromCharCode(character));
+    };
+    setPassword(passwordCharacters.join(''));
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    generatePassword(inputValue, includeUppercase, includeNumbers, includeSymbols);
   };
 
   return(
     <div className="password-container">
       <h1>Password Generator</h1>
-      <h3>password</h3>
+      <h3>{password}</h3>
       <form className="password-c-form" onSubmit={handleSubmit}>
         <div>
           <label htmlFor="">Number of characters</label>
